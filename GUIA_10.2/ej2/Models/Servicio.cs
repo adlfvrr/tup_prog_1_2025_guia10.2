7bbe0904d0853c17e313db7b52ba5497b4f03779
adlfvrr <adolfover4@gmail.com>
2025-06-19 20:45:37 -0300

    -

---
+++ GUIA_10.2/ej2/Models/Servicio.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej2.Models
+{
+    internal class Servicio
+    {
+        public string[] Nombres;
+        public int[] Libretas;
+        public double[] Notas;
+        public int Cantidad;
+        public Servicio()
+        {
+            Cantidad = 0;
+            Nombres = new string[50];
+            Notas = new double[50];
+            Libretas = new int[50];
+        }
+        private void IntercambioCompleto(int i, int j)
+        {
+            int aux = Libretas[i];
+            Libretas[i] = Libretas[j];
+            Libretas[j] = aux;
+            //
+            double aux2 = Notas[i];
+            Notas[i] = Notas[j];
+            Notas[j] = aux2;
+            //
+            string aux3 = Nombres[i];
+            Nombres[i] = Nombres[j];
+            Nombres[j] = aux3;
+        }
+        public bool RegistrarAlumno(int libreta, string nombre, double nota)
+        {
+            bool registrar = false;
+            if (Cantidad < Notas.Length)
+            {
+                registrar = true;
+                Libretas[Cantidad] = libreta;
+                Notas[Cantidad] = nota;
+                Nombres[Cantidad] = nombre;
+                Cantidad++;
+            }
+            return registrar;
+        }
+        public double CalcularPromedio()
+        {
+            double prom = 0;
+            double ac = 0;
+            for (int i = 0; i < Cantidad; i++)
+            {
+                ac += Notas[i];
+            }
+            prom = 1.0 * (ac / (double)Cantidad);
+            return prom;
+        }
+        public int CalcularAlumnoMayorNota()
+        {
+            int indice = -1;
+            int mayor = 0;
+            for (int i = 0; i < Cantidad; i++)
+            {
+                if (Notas[i] > Notas[mayor])
+                {
+                    mayor = i;
+                }
+            }
+            return mayor;
+        }
+        public int CalcularAlumnoMenorNota()
+        {
+            int indice = -1;
+            int menor = 0;
+            for (int i = 0; i < Cantidad; i++)
+            {
+                if (Notas[i] < Notas[menor])
+                {
+                    menor = i;
+                }
+            }
+            return menor;
+        }
+        public void OrdenarAlumnosPorLibreta()
+        {
+            for (int i = 0; i < Cantidad; i++)
+            {
+                for (int j = i + 1; j < Cantidad; j++)
+                {
+                    if (Libretas[i] > Libretas[j])
+                    {
+                        IntercambioCompleto(i, j);
+                    }
+                }
+                Console.WriteLine($"{i + 1}, Nombre:{Nombres[i]}, Numero de libreta {Libretas[i]}, Nota {Notas[i]}");
+            }
+            Console.ReadKey();
+        }
+        public int BuscarPorLibreta(int libreta)
+        {
+            int indice = -1;
+            for (int i = 0; i < Cantidad; i++)
+            {
+                if (Libretas[i] == libreta)
+                {
+                    indice = i;
+                }
+            }
+            return indice;
+        }
+        public int BuscarValor(int buscado)
+        {
+            int valor = -1;
+            for (int i = 0; i < Cantidad; i++)
+            {
+                if (Libretas[i] == buscado)
+                {
+                    valor = Libretas[i];
+                }
+            }
+            return valor;
+        }
+        public int[] ListarAlumnosSuperaronElPromedio(out int cantidadMayores)
+        {
+            int[] mayores = new int[Cantidad];
+            cantidadMayores = 0;
+            double promedio = CalcularPromedio();
+            for (int i = 0; i < Cantidad; i++)
+            {
+                if (Notas[i] >= promedio)
+                {
+                    mayores[cantidadMayores] = Libretas[i];
+                    cantidadMayores++;
+                }
+            }
+            return mayores;
+        }
+    }
+}
---
+++ GUIA_10.2/ej2/Program.cs
@@ -0,0 +1,99 @@
+﻿using ej2.Models;
+namespace ej2
+{
+    internal class Program
+    {
+        static private Servicio servicio = new Servicio();
+        private static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese las siguientes opciones:\n1- Registrar un alumno\n2- Mostrar alumno con mayor y menor nota\n3- Mostrar el listado de alumnos ordenados por numero de libreta\n4- Mostrar promedio general y el listado que superaron el promedio\n5- Buscar alumno por numero de libreta\n(otro)- Salir");
+            int op = Convert.ToInt32(Console.ReadLine());
+            return op;
+        }
+        private static void MostrarPantallaSolicitarAlumno()
+        {
+            Console.Write("Ingrese nombre del alumno: ");
+            string nombre = Console.ReadLine();
+            Console.Write($"Ingrese numero de libreta de {nombre}: ");
+            int libreta = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Ingrese nota de {nombre}: ");
+            double nota = Convert.ToDouble(Console.ReadLine());
+            bool registro = servicio.RegistrarAlumno(libreta, nombre, nota);
+            if (registro == true)
+            {
+                Console.WriteLine($"{nombre}, datos registrados correctamente");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Capacidad superada");
+                Console.ReadKey();
+            }
+        }
+        private static void MostrarPantallaMostrarAlumnoMayorYMenorNota()
+        {
+            if (servicio.Cantidad > 0)
+            {
+                int indiceMayor = servicio.CalcularAlumnoMayorNota();
+                int indiceMenor = servicio.CalcularAlumnoMenorNota();
+                Console.WriteLine($"El alumno con mayor nota es {servicio.Nombres[indiceMayor]}, con una nota de {servicio.Notas[indiceMayor]}\nEl alumno con menor nota es {servicio.Nombres[indiceMenor]}, con una nota de {servicio.Notas[indiceMenor]}");
+                Console.ReadKey();            
+            }
+            else
+            {
+                Console.WriteLine("No se ingresaron alumnos");
+                Console.ReadKey();
+            }
+        }
+        private static void MostrarPantallaListadoOrdenadosPorLibreta()
+        {
+           servicio.OrdenarAlumnosPorLibreta();
+        }
+        private static void MostrarPantallaPromedioYListadoSuperaronPromedio()
+        {
+            double prom = servicio.CalcularPromedio();
+            Console.WriteLine($"El promedio de las notas es de {prom:f2}");
+            int[] listaAlumnosSuperioresAlPromedio = servicio.ListarAlumnosSuperaronElPromedio(out int cantidadMayores);
+            Console.WriteLine("Los alumnos mayores al promedio son:");
+            for(int i = 0; i < cantidadMayores; i++)
+            {
+                Console.WriteLine($"{servicio.Nombres[i]} ({servicio.Notas[i]})");
+            }
+            Console.ReadKey();
+        }
+        private static void MostrarPantallaDatosAlumno()
+        {
+            Console.Write("Ingrese numero de libreta del alumno: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int indice = servicio.BuscarPorLibreta(numero);
+            if(indice != -1)
+            {
+                Console.WriteLine($"Nombre: {servicio.Nombres[indice]}, Nota: {servicio.Notas[indice]}");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Alumno no encontrado");
+                Console.ReadKey();
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool menu = true;
+            while (menu)
+            {
+                int op = MostrarPantallaSolicitarOpcionMenu();
+                switch (op)
+                {
+                    case 1: MostrarPantallaSolicitarAlumno();break;
+                    case 2: MostrarPantallaMostrarAlumnoMayorYMenorNota();break;
+                    case 3: MostrarPantallaListadoOrdenadosPorLibreta();break;
+                    case 4: MostrarPantallaPromedioYListadoSuperaronPromedio(); break;
+                    case 5: MostrarPantallaDatosAlumno();break;
+                    default: menu = false;break;
+                }
+            }
+        }
+    }
+}
